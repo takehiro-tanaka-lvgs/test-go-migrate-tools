46340c6bdee3e4fbdcdc25b1025d6faf8717d075
takehiro-tanaka-lvgs <97871541+takehiro-tanaka-lvgs@users.noreply.github.com>
2024-03-28 21:18:30 +0900

    chore: add user roles tables

---
+++ sql-migrate/migrations/20240328121546-add_user_roles_tables.sql
@@ -0,0 +1,11 @@
+
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS user_roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+-- +migrate Down
+DROP TABLE IF EXISTS user_roles;
